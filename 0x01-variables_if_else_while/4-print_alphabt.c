43a7c8a6de967e8e45e59a95319613e25e166da5
Emmanuel Lubwama <lubwamaemmanuel1@gmail.com>
2022-09-09 05:58:27 -0700

    Not e and q

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -8,10 +8,10 @@ int main(void)
 {
 	char ltr;
 
-	for (ltr = 'a'; ltr <= 'z'; ltr++){
-		if(ltr != 'e' && ltr != 'q'){
+	for (ltr = 'a'; ltr <= 'z'; ltr++)
+	{
+		if (ltr != 'e' && ltr != 'q')
 			putchar(ltr);
-		}
 	}
 	putchar('\n');
 	return (0);
