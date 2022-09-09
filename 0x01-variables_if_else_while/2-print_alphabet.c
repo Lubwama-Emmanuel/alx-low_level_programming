5e78894930e8b91633da1351e3317ed62a8e1e5d
Emmanuel Lubwama <lubwamaemmanuel1@gmail.com>
2022-09-09 01:08:16 -0700

    lower alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - main fxn
+ * ltr: letters
+ * Return: main fxn returns zero
+ */
+int main(void)
+{
+	char ltr;
+
+	for (ltr = 'a'; ltr >= 'z'; ltr++)
+		putchar(ltr);
+	return (0);
+}
