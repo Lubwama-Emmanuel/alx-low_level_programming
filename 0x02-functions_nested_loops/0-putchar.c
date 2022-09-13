34dd0d2cdcc2d9dcca377698ad40162ae02d3343
Emmanuel Lubwama <lubwamaemmanuel1@gmail.com>
2022-09-13 07:46:36 -0700

    _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,13 +1,18 @@
 #include <stdio.h>
 #include <string.h>
 #include "main.h"
-
+/**
+ * main - main fxn 
+ * str: character
+ *
+ * Return: return zero
+ */
 int main(void)
 {
 	char str[] = "_putchar";
 	int ch;
 
-	for (ch = 0; ch < 9; ch++)
+	for (ch = 0; ch <= 8; ch++)
 		_putchar(str[ch]);
 	_putchar('\n');
 
