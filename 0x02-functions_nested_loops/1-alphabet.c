84ca8117173336b6f6160679dcedab3226add968
Emmanuel Lubwama <lubwamaemmanuel1@gmail.com>
2022-09-13 08:01:27 -0700

    Print alpha

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_alphabet - prints alphabet lowercase
+ * 
+ */
+void print_alphabet(void)
+{
+	char x = 'a';
+
+	while (ch <= 'z')
+	{
+		_putchar(ch);
+		ch++;
+	}
+
+}
