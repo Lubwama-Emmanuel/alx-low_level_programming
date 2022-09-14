bae6dc629ea9daa02cc6c603e7c044ad8c82d3de
Emmanuel Lubwama <lubwamaemmanuel1@gmail.com>
2022-09-14 02:10:45 -0700

    9x9

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+void jack_bauer(void)
+{
+	int i=0, j, k;
+
+	while (i<10)
+	{
+		j=0;
+		
+
---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * times_table - 9x9 timetable
+ *
+ */
+void times_table(void)
+{
+	int i = 0, j, res;
+
+	while (i < 10)
+	{
+		j = 0;
+		while (j < 10)
+		{
+			res = i * j;
+			_putchar(res + '0');
+			_putchar(' ');
+			++j;
+		}
+		_putchar("\n");
+		++i;
+	}
+}
