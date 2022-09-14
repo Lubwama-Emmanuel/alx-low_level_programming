fa99ac262fdf15529b033b6df29fc40bac3e13e5
Emmanuel Lubwama <lubwamaemmanuel1@gmail.com>
2022-09-14 00:00:24 -0700

    print sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_sign - prints the sign of a number
+ * @n: test number
+ *
+ * Return: returns as follows
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	} else if (n < 0)
+	{
+		_putchar('-');
+		return (0);
+	} else
+		return (-1);
+}
