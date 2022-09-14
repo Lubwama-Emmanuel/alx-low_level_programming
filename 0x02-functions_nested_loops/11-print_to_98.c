029fe12b54ae8a75981374cd3362ccd705b8e0c2
Emmanuel Lubwama <lubwamaemmanuel1@gmail.com>
2022-09-14 05:33:45 -0700

    print to 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -11,31 +11,34 @@ void print_to_98(int n)
 	{
 		for (i = n; i <= 98; i++)
 		{
+			printf("%d", i);
 			if (i != 98)
 			{
-				printf("%d, ", i);
+				printf(",");
+				printf(" ");
 			}
-			printf("%d", i);
 		}
 	} else if (n > 98)
 	{
 		for (i = n; i >= 98; i--)
 		{
+			printf("%d", i);
 			if (i != 98)
 			{
-				printf("%d, ", i);
+				printf(",");
+				printf(" ");
 			}
-			printf("%d", i);
 		}
 	} else
 	{
 		for (i = n; i <= 98; i++)
 		{
+			printf("%d", i);
 			if (i != 98)
 			{
-				printf("%d, ", i);
+				printf(",");
+				printt(" ");
 			}
-			printf("%d", i);
 		}
 	}
 }
