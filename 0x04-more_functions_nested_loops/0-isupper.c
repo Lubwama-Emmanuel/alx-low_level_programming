6561d5470c9b4bfcc7979fe39f47efaef8367d8a
Emmanuel Lubwama <lubwamaemmanuel1@gmail.com>
2022-09-15 01:32:08 -0700

    isupper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,16 @@
+#include <ctype.h>
+/**
+ * _isupper - function that is a letter is capital
+ * @c: letter to test
+ *
+ * Return: returns 1 or 0
+ *
+ */
+int _isupper(int c)
+{
+	if (isupper(c))
+	{
+		return (1);
+	}
+	return (0);
+}
