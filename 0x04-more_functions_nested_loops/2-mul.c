d7edec32b115fa37580a70de5d3381d1990d8e19
Emmanuel Lubwama <lubwamaemmanuel1@gmail.com>
2022-09-15 01:53:39 -0700

    Multipying two numbers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * mul - function that multiplies
+ *@a: first int
+ * @b: second int
+ *
+ * Return: returns result
+ */
+int mul(int a, int b)
+{
+	int result;
+
+	result = (a * b);
+	return (result);
+}
