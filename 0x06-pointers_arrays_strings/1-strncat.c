890f1c97831891d6e5b780a96396d1be398de499
Emmanuel Lubwama <lubwamaemmanuel1@gmail.com>
2022-09-21 22:49:57 -0700

    Strncat

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,22 @@
+#include <string.h>
+#include <stdio.h>
+/**
+ * _strncat - concats strings with a given number
+ * @dest: destination string
+ * @src: source string
+ * @n: number of characters to copy from the src
+ *
+ * Return: return the destination string
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int i, j;
+
+	j = strlen(dest);
+	for (i = 0; i < n; i++)
+	{
+		dest[j] = src[i];
+		j++;
+	}
+	return (dest);
+}
