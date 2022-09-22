3e681bea12abe8d8f5bd251f3a4692277b9c48c8
Emmanuel Lubwama <lubwamaemmanuel1@gmail.com>
2022-09-21 23:56:48 -0700

    strcmp

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -5,14 +5,38 @@
  * @s1: first string
  * @s2: second string to compare to the first
  *
- * Return: returns an int
+ * Return: returns an int as follows
+ *	0, if the s1 and s2 are equal
+ *	a negative value if s1 is less than s2;
+ *	a positive value if s1 is greater than s2;
  */
 int _strcmp(char *s1, char *s2)
 {
-	while (*s1 && (*s1 == *s2))
+	int i = 0, j;
+
+	/**
+	 * iterate throught src and
+	 * compare it with dest
+	 */
+	for (i = 0; s1[i] != '\0' && s2[i] != '\0'; i++)
 	{
-		s1++;
-		s2++;
+		/**
+		 * if they differ by a single character
+		 * don't iterate further
+		 */
+		if (s1[i] != s2[i])
+		{
+			/**
+			 * return the difference btn
+			 * them
+			 */
+			j = s1[i] - s2[i];
+			break;
+		}
+		else
+		{
+			j = s1[i] - s2[i];
+		}
 	}
-	return (*(const unsigned char *)s1 - *(const unsigned char *)s2);
+	return (j);
 }
