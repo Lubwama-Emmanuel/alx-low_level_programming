10e74a29c289fd47b473f172abeb36213c55f982
Emmanuel Lubwama <lubwamaemmanuel1@gmail.com>
2022-09-29 18:24:48 -0700

    Done

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -8,7 +8,9 @@
 int _strlen_recursion(char *s)
 {
 	if (!*s)
+	{
 		return (0);
+	}
 	return (1 + _strlen_recursion(++s));
 }
 
@@ -22,9 +24,13 @@ int _strlen_recursion(char *s)
 int p1(char *s, int i)
 {
 	if (i < 1)
+	{
 		return (1);
+	}
 	if (*s == *(s + 1))
+	{
 		return (p1(s + 1, i - 2));
+	}
 	return (0);
 }
 
