71f80509432eb5323034d30fb976e78c2d995215
Emmanuel Lubwama <lubwamaemmanuel1@gmail.com>
2022-10-05 12:18:14 -0700

    Done

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - main fxn
+ * @argc: counts the arguments
+ * @argv: arguments
+ *
+ * Return: 0(success exit)
+ */
+int main(int argc, char __attribute__((unused)) *argv[])
+{
+	printf("%d\n", (argc - 1));
+
+	return (0);
+}
