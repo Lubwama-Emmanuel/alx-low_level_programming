b4649d48b9db6198043eea67aeb324cb2abb81f8
Emmanuel Lubwama <lubwamaemmanuel1@gmail.com>
2022-10-05 12:12:29 -0700

    Done

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - main fxn
+ * @argc: takes in arguments for main
+ * @argv: array of the @argc
+ *
+ * Return: main returns zero
+ */
+int main(int __attribute__((unused)) argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
