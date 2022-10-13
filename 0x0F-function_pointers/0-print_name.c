dc5d9ee46f2c26ba2dc7b75215cfcdb575c3c45e
Emmanuel Lubwama <lubwamaemmanuel1@gmail.com>
2022-10-12 23:16:41 -0700

    Done

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,1 +1,14 @@
-
+#include "function_pointers.h"
+/**
+ * print_name - function to print a name
+ *
+ * @name: name to print
+ * @f: function to pointer that doesn't return anything
+ *
+ * Return: Always 0 sucess
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	if (name && f)
+		f(name);
+}
