2e45cc7ae5214e69c64d4fb6c3392d0f157e77cb
Emmanuel Lubwama <lubwamaemmanuel1@gmail.com>
2022-10-21 05:14:20 -0700

    Done

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,3 +1,18 @@
-#include "main.h"
+#include "lists.h"
+/**
+ * list_len - Function to return length of singly list
+ * @h: singly list
+ *
+ * Return: returns the length
+ */
+size_t list_len(const list_t *h)
+{
+	size_t length = 0;
 
-:
+	while (h != NULL)
+	{
+		h = h->next;
+		length++;
+	}
+	return (length);
+}
