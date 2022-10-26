f0e54620137e8c330c4880b9d33b0c47b5f098cb
Emmanuel Lubwama <lubwamaemmanuel1@gmail.com>
2022-10-26 00:58:51 -0700

    Done

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+  * listint_len - function that prints number of elements ina list
+  *
+  * @h: head of the list
+  *
+  * Return: returns the number of elements
+  */
+
+size_t listint_len(const listint_t *h)
+{
+	size_t len = 0;
+
+	while (h != NULL)
+	{
+		len++;
+	}
+	return (len);
+}
